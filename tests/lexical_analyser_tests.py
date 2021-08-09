4d03dd1ac0366277065b4a494437acad9a36fb82
Daniel Moraes <danielbibit@gmail.com>
2021-08-09 18:49:22 -0300

    Implement token generator using scanner class

--- tests/lexical_analyser_tests.py
+++ tests/lexical_analyser_tests.py
@@ -4,7 +4,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 import compiler.lexical as lexical
 
 test_strings = [
-    '23', '2.4a', '2.8e10', '3.5e-8','2E-10',
+    '2334\n', '2.4a\n', '2.8e10\n', '3.5e-8\n','2E-10\n',
     '\'>\'', '\"Daniel2.\"',
     'Animal', 'Dan_1',
     '{daniel+1}',
@@ -14,5 +14,19 @@ test_strings = [
     '+-',
 ]
 
-for s in test_strings:
-    lexical.scanner(s)
+# f = open('tokens_test.mgol')
+f = open('source_test.mgol')
+
+with f:
+    f1 = f.read()
+
+# for i in range(5):
+#     print(repr(f1[i]))
+
+scanner = lexical.Scanner(f1)
+for token in scanner.next():
+    print(token)
+
+# for s in test_strings:
+#     scanner = lexical.Scanner(s)
+#     scanner.next()
