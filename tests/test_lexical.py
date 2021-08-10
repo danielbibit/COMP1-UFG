276f5f1ff68072b11115edd5bdb231035f1f7009
Daniel Moraes <danielbibit@gmail.com>
2021-08-10 18:24:52 -0300

    Add new tests

--- tests/test_lexical.py
+++ tests/test_lexical.py
@@ -1,5 +1,4 @@
 import os, sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import unittest
 
@@ -29,6 +28,11 @@ class TestLexical(unittest.TestCase):
             '<\n', '>\n', '<=\n', '<>\n','>=\n',
         ]
 
+        self.full_strings = [
+            'se(B>2);B<-B+1;\n',
+        ]
+
+
     def test_single_tokens(self):
         everything = self.strings_num + self.strings_lit + self.strings_id + self.strings_coments + self.strings_relacionais
 
@@ -37,18 +41,39 @@ class TestLexical(unittest.TestCase):
             for token in s.next():
                 self.assertNotEqual(token.classe, 'ERRO')
 
+
     def test_eof(self):
         for string in self.strings_num:
-            last_token = token.Token('','','')
-
             s = scanner.Scanner(string)
-            for t in s.next():
-                last_token = t
 
-            self.assertEqual(last_token.classe, 'EOF')
+            tokens = [t for t in s.next()]
 
-    def test_valid_code(self):
+            self.assertEqual(tokens[-1].classe, 'EOF')
+
+        #Test whole source code
         f = open('tests/valid_source_test.mgol')
+        with f:
+            f1 = f.read()
+
+        s = scanner.Scanner(f1)
+
+        tokens = [t for t in s.next()]
+
+        self.assertEqual(tokens[-1].classe, 'EOF')
+
+
+    def test_valid_source_code(self):
+        f = open('tests/valid_source_test.mgol')
+        with f:
+            f1 = f.read()
+        s = scanner.Scanner(f1)
+        for token in s.next():
+            self.assertNotEqual(token.classe, 'ERRO')
+
+
+    @unittest.expectedFailure
+    def test_invalid_source_code(self):
+        f = open('tests/invalid_source_test.mgol')
         with f:
             f1 = f.read()
         s = scanner.Scanner(f1)
