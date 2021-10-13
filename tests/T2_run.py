2ec75b9f8070a9f2bcfc5caedc8c2adff1b7cafb
Daniel Moraes <danielbibit@gmail.com>
2021-10-13 14:41:13 -0300

    Fix import path

--- tests/T2_run.py
+++ tests/T2_run.py
@@ -1,11 +1,12 @@
 import os, sys
 import logging as log
 
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import compiler.lexical.scanner as scanner
 import compiler.syntax as syntax
 import compiler.syntax.analyzer as analyzer
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 # log.basicConfig(level=log.DEBUG)
 
