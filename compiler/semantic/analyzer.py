8c2ffdb4049bedc8e91a6bfe5da65fe5a105db3e
Daniel Moraes <danielbibit@gmail.com>
2021-10-29 03:36:13 -0300

    Start semantic analyzer implementation

---
+++ compiler/semantic/analyzer.py
@@ -0,0 +1,57 @@
+import logging as log
+
+class Analyzer():
+    def semantic_analyzer(state, rule):
+        if state == '5':
+            pass
+        elif state == '6':
+            pass
+        elif state == '7':
+            pass
+        elif state == '8':
+            pass
+        elif state == '9':
+            pass
+        elif state == '10':
+            pass
+        elif state == '11':
+            pass
+        elif state == '13':
+            pass
+        elif state == '14':
+            pass
+        elif state == '15':
+            pass
+        elif state == '16':
+            pass
+        elif state == '17':
+            pass
+        elif state == '19':
+            pass
+        elif state == '20':
+            pass
+        elif state == '21':
+            pass
+        elif state == '22':
+            pass
+        elif state == '23':
+            pass
+        elif state == '25':
+            pass
+        elif state == '26':
+            pass
+        elif state == '27':
+            pass
+        elif state == '33':
+            pass
+        elif state == '34':
+            pass
+        elif state == '35':
+            pass
+        elif state == '36':
+            pass
+        elif state == '37':
+            pass
+
+
+
